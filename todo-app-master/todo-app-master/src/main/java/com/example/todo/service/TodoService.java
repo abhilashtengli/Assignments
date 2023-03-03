1f9823c4a4e4b79d8d45f5e0c357fb08a7f337c5
Abhilash Tengli <122851437+abhilashtengli@users.noreply.github.com>
2023-03-03 12:08:07 +0530

    Add files via upload

---
+++ todo-app-master/todo-app-master/src/main/java/com/example/todo/service/TodoService.java
@@ -0,0 +1,134 @@
+package com.example.todo.service;
+
+
+import com.example.todo.model.Todo;
+import com.example.todo.repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+// import java.util.function.Predicate;
+
+
+@Service
+public class TodoService implements  ITodoService {
+
+    private static List<Todo> todos=new ArrayList<>(); // In-memory database
+    private static int todoCount=0;
+
+    @Autowired
+    TodoRepository todoRepository;
+
+
+    // static {
+    //     todos.add(new Todo(++todoCount,"Learn Spring Boot",false));
+    //     todos.add(new Todo(++todoCount,"Visit Grocery Store",false));
+    //     todos.add(new Todo(++todoCount,"Learn AWS",false));
+    //     todos.add(new Todo(++todoCount,"Swimming",false));
+    //     todos.add(new Todo(++todoCount,"Yoga",false));
+    // }
+
+    // @Override
+    // public List<Todo> findAll() { // Business Logic
+    //     return todos;
+    // }
+
+    @Override
+    public List<Todo> findAll(){
+        return todoRepository.findAll();
+    }
+
+    // @Override
+    // public Todo findById(int id) { // business LOgic
+    //     Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+    //     Todo todo = todos.stream().filter(predicate).findFirst().get();
+    //     return todo;
+    // }
+     
+    @Override
+    public Todo findById(int id){
+        return todoRepository.findById(id).get();
+    } 
+
+    // @Override
+    // public void addTodo(Todo todo) {
+    //     todos.add(todo);
+    // }
+
+    @Override
+    public void addTodo(Todo todo){
+        todoRepository.save(todo);
+    }
+
+    // @Override
+    // public void deleteTodo(int id) {
+    //     Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+    //     todos.removeIf(predicate);
+    // }
+
+    public void deleteTodo(int id){
+        todoRepository.deleteById(id);
+    }
+
+    // @Override
+    // public void updateTodo(int id,Todo newTodo) {
+    //     //step 1: find todo to be update
+    //     //Step 2: update todo
+
+    //     Todo todo=findById(id);// step 1
+    //     todo.setId(newTodo.getId());
+    //     todo.setTitle(newTodo.getTitle());
+    //     todo.setStatus(newTodo.isStatus());
+    // }
+
+    @Override
+    public void updateTodo(int id, Todo newTodo){
+
+        Todo todo=findById(id);// step 1
+        todo.setId(newTodo.getId());
+        todo.setTitle(newTodo.getTitle());
+        todo.setStatus(newTodo.isStatus());
+        todoRepository.save(newTodo);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//return new ResponseEntity<>(todoService.getTodoById(todoId), HttpStatus.OK);
+//return new ResponseEntity<>(todo, HttpStatus.CREATED);
+/*
+
+{
+    "id":100,
+    "title": "Buy eggs from supermarket",
+    "status": false
+}
+
+Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+
+Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+
+ */
